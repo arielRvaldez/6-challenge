9fc50638fac422b074379d28fda5150521d374cb
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2023-12-10 22:10:54 -0600

    added API key to JS

---
+++ assets/javascript.js
@@ -0,0 +1,11 @@
+var APIkey = "e5ef4fa9d75ce2035e478b8919ac7928";
+var currentCity = "";
+var lastCity = "";
+
+var getCurrentConditions = (event) => {
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+    }
+    return response;
+}
+
