03b23d5bb21262c17d9bcd730ce1ce5d4a804da5
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2023-12-11 00:08:24 -0600

    added list of weather conditions

--- assets/javascript.js
+++ assets/javascript.js
@@ -32,22 +32,18 @@ var getCurrentConditions = (event) => {
         $('#header-text').text(response.name);
         //HTML for searched results//
         var currentWeatherHTML = 
-            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<><img src="${currentWeatherIcon}">
+            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}">
             </h3>
             <ul class="list-unstyled">
             <li>Temperature: ${response.main.temp}&#8457;</li>
             <li>Humidity: ${response.main.humidity}%</li>
             <li>Wind Speed: ${response.wind.speed} mph</li>
-            <li id="uvIndex">UV Index:</li>
         </ul>;
-
         $('#current-weather').html(currentWeatherHTML);
-        let latitude = response.coord.lat;
-        let longitude = response.coord.lon;
-        let uvQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q="
-    }
-
-    }
+            return response.json();
+        }
+    );
+        
 
 
 
