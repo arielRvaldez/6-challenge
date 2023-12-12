5ec332ec5c4f1d09507bf10f27a9a131ae534e8f
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2023-12-11 22:33:04 -0600

    added render to CSS

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,17 +1,18 @@
 var APIkey = "e5ef4fa9d75ce2035e478b8919ac7928";
 var currentCity = "";
 var lastCity = "";
-var currentweather =document.querySelector('#current-weather')
-var fivedayforecast =document.querySelector('#5day-forecast')
-renderCities();
-getCurrentConditions();
+var currentweather = document.querySelector('#current-weather')
+var fivedayforecast = document.querySelector('#five-day-forecast')
+var searchform = document.querySelector('#search-form')
+//renderCities();
+//getCurrentConditions();
 
 //current weather display//
-var getCurrentConditions = function (event) {
+function getCurrentConditions(event) {
     event.preventDefault();
-
+    console.log('testing')
     let city = $('#search-city').val();
-    currentCity= $('#search-city').val();
+    currentCity = $('#search-city').val();
     //OpenWeather URL with imperial units with JSON//
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIkey;
 
@@ -21,61 +22,96 @@ var getCurrentConditions = function (event) {
                 console.log(response);
                 response.json().then(function (data) {
                     console.log(data);
-                    displayCities(data, city);
+                    displayWeather(data, city);
                 });
             }
-    }
-    //local storage//
-    function .then((response) {
-        saveCity(city);
-        $('#search-error').text("");
-        //current weather icons//
-        var currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-        $('#header-text').text(response.name);
-        
+        }
+        )
+}
+
+function displayWeather(data, city) {
+    renderCurrent(city, data)
+}
+
+function renderCurrent(city, weather) {
+    var temp = weather.main.temp;
+    var wind = weather.wind.speed;
+    var humidity = weather.main.humidity;
+    console.log(temp, wind, humidity);
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var heading = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+    card.setAttribute('class', 'card');
+    cardBody.setAttribute('class', 'card-body');
+    card.append(cardBody);
+
+    heading.setAttribute('class', 'h3 card-title');
+    tempEl.setAttribute('class', 'card-text');
+    windEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text');
+
+    heading.textContent = `${city} `;
+    tempEl.textContent = `Temp: ${temp}°F`;
+    windEl.textContent = `Wind: ${wind} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+    currentweather.innerHTML= "";
+    currentweather.append(card);
+}
+//local storage//
+//function .then((response) {
+//     saveCity(city);
+//     $('#search-error').text("");
+//     //current weather icons//
+//     var currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+//     $('#header-text').text(response.name);
+
+// })
+
+//search button event listener//
+//$(document).ready(function(){
+//search button*******************************// 
+// $("#search-button").on("click", function () {
+//   //user inputs city//
+//   var searchTerm = $("#search-city").val();
+//   $("#search-city").val("");
+//   weatherFunction(searchTerm);
+//   weatherForecast(searchTerm);
+// });
+
+//pulls prior searched city//
+var history = JSON.parse(localStorage.getItem("city-results")) || [];
+
+//creates row for searched cities
+for (var i = 0; i < history.length; i++) {
+    createRow(history[i]);
+}
+//lists prior searched cities in order
+function createRow(text) {
+    var listItem = $("<li>").addClass("list-group-item").text(text);
+    $(".history").append(listItem);
+}
+$(".history").on("click", "li", function () {
+    weatherFunction($(this).text());
+    weatherForecast($(this).text());
+});
+
+//when user chooses prior searched city
+function weatherFunction(searchTerm) {
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=e5ef4fa9d75ce2035e478b8919ac7928"
     })
+}
+//select previous searched city//
+//history.addEventListener('click', buttonClickHandler);
 
-    //search button event listener//
-    $(document).ready(function(){
-        //search button*******************************// 
-        $("#search-button").on("click", function () {
-          //user inputs city//
-          var searchTerm = $("#search-city").val();
-          $("#search-city").val("");
-          weatherFunction(searchTerm);
-          weatherForecast(searchTerm);
-        });
-
-    //pulls prior searched city//
-    var history = JSON.parse(localStorage.getItem("city-results")) || [];
-
-    //creates row for searched cities
-    for (var i = 0; i < history.length; i++) {
-        createRow(history[i]);
-    }
-    //lists prior searched cities in order
-    function createRow(text) {
-        var listItem = $("<li>").addClass("list-group-item").text(text);
-        $(".history").append(listItem);
-    }
-    $(".history").on("click", "li", function () {
-        weatherFunction($(this).text());
-        weatherForecast($(this).text());
-      });
-
-    //when user chooses prior searched city
-    function weatherFunction(searchTerm) {
-        $.ajax ({
-          type: "GET",
-          url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=e5ef4fa9d75ce2035e478b8919ac7928"
-        })
-    
-    //select previous searched city//
-    history.addEventListener('click', buttonClickHandler);
-    
-
-    //lists the cities//
-    renderCities();
-    //**currentweather for searched city//
-    currentweather(event);
-    //header for searched city//
+searchform.addEventListener('submit', getCurrentConditions);
+//lists the cities//
+//renderCities();
+//**currentweather for searched city//
+//currentweather(event);
+//header for searched city//
